585dee432a3bd08c1caa5d3fd622711c727359b1
Abhishek Choudhari <abhishekc97@gmail.com>
2023-04-22 02:20:56 +0530

    Added redux data store for user login and logout functionality

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Login from "../src/pages/Login/Login";
 import Register from "./pages/Register/Register";
+import { useSelector } from "react-redux";
 
 function App() {
+    const { isAuthenticated } = useSelector((state) => state.root);
+
     return (
         <BrowserRouter>
             <div className={styles.App}>
                 <Routes>
-                    <Route path="/" element={<Home />} />
+                    <Route
+                        path="/"
+                        element={<Home isAuthenticated={isAuthenticated} />}
+                    />
                     <Route path="/login" element={<Login />} />
                     <Route path="/register" element={<Register />} />
                 </Routes>
